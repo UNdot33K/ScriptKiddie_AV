9ed6c6a1463ee465224d671a517a43275d922401
Un.33k <62697009+UNdot33K@users.noreply.github.com>
2023-02-19 04:16:34 +0200

    Update CMP.py

--- v1_2_1b/CMP.py
+++ v1_2_1b/CMP.py
@@ -1,3 +1,22 @@
+:: Copyright (c) 2023 Un.33k (www.github.com/UNdot33K) 
+
+:: Permission is hereby granted, free of charge, to any person obtaining a copy 
+:: of this software and associated documentation files (the "Software"), to deal 
+:: in the Software without restriction, including without limitation the rights 
+:: to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+:: of the Software, and to permit persons to whom the Software is furnished to do so, 
+:: subject to the following conditions:
+
+:: The above copyright notice and this permission notice shall be included in all 
+:: copies or substantial portions of the Software.
+
+:: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+:: INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+:: PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+:: HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+:: OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+:: SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import sys
 import difflib
 import ctypes
@@ -30,4 +49,5 @@ if __name__ == '__main__':
         print('Copyright 2023: UN.33K - Compare files: filename1 filename2 outputfile')
         sys.exit(1)
     compare_files(sys.argv[1], sys.argv[2], sys.argv[3])
+    
 
