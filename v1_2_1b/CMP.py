79b4c7445dac2e0ed85e93717c0881f81f526c72
Un.33k <62697009+UNdot33K@users.noreply.github.com>
2023-02-23 07:55:11 +0200

    Update CMP.py

--- v1_2_1b/CMP.py
+++ v1_2_1b/CMP.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Un.33k (www.github.com/UNdot33K) 
+# Copyright (c) 2023 Un.33k (www.github.com/UNdot33K)
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy 
 # of this software and associated documentation files (the "Software"), to deal 
@@ -17,37 +17,50 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+
 import sys
 import difflib
 import ctypes
 
-# Function to print color text in the command line
+# Function to print in color in the terminal
 def set_color(color):
     std_out_handle = ctypes.windll.kernel32.GetStdHandle(-11)
     ctypes.windll.kernel32.SetConsoleTextAttribute(std_out_handle, color)
 
-# comparing files using the difflib library
-def compare_files(file1, file2, outputfile):
-    with open(file1, 'r', encoding='IBM866') as f1, open(file2, 'r', encoding='IBM866') as f2:
-        f1_contents = f1.readlines()
-        f2_contents = f2.readlines()
+# Function definition.
 
-        difference = list(difflib.unified_diff(f1_contents, f2_contents, fromfile=file1, tofile=file2, n=0))
-        if difference:
-            set_color(0x00f0) # white background black letters
-            print('\a') # Make a beep sound
-            print("Integrity: The files are not identical, differences written")
-            print(f"to {outputfile}")
-            set_color(0x0007) # set to black background grey letters
-            with open(outputfile, 'w', encoding='IBM866') as outfile:
-                outfile.writelines(difference)
-        else:
-            print('Integrity: The files are identical.')
+def compare_files(file1, file2, outputfile, chunk_size=128000):
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        while True:
+            # Read a chunk from each file
+            chunk1 = f1.read(chunk_size)
+            chunk2 = f2.read(chunk_size)
+
+            # If either file has reached its end, stop comparing
+            if not chunk1 or not chunk2:
+                break
+
+            # Generate the differences between the chunks
+            diff = difflib.unified_diff(chunk1.splitlines(), chunk2.splitlines(), lineterm='')
+            if diff:
+                   set_color(0x00f0) # white background black letters
+                   print('\a') # Make a beep sound
+                   print("Integrity: The files are not identical, differences written") 
+                   print(f"to {outputfile}")
+                   set_color(0x0007) # reset to black background gray letters
+
+                   # Open file for writting in binary mode.
+                   with open(outputfile, 'wb') as outfile:
+                       outfile.writelines(diff)
+            else:
+                print('Integrity: The files are identical.')
 
 if __name__ == '__main__':
+
+    # Check if parameters have been passed to the command line.
     if len(sys.argv) != 4:
         print('Copyright 2023: UN.33K - Compare files: filename1 filename2 outputfile')
         sys.exit(1)
-    compare_files(sys.argv[1], sys.argv[2], sys.argv[3])
     
-
+    # Calling the comparison function with the parameters from the command line.
+    compare_files(sys.argv[1], sys.argv[2], sys.argv[3])
