9242ae0d3698e73feaf679698dd87c987dd6534f
Un.33k <62697009+UNdot33K@users.noreply.github.com>
2023-02-27 21:29:19 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -80,7 +80,7 @@ echo.
 echo Copying files...
 echo.
 
-copy /y %userprofile%\Desktop\ScriptKiddie_AV-main\v1_2_1b\main.bat %userprofile%\Desktop\SC_AV\
+copy /y %userprofile%\Desktop\ScriptKiddie_AV-main\v1_2_5b\main.bat %userprofile%\Desktop\SC_AV\
 
 copy /y %userprofile%\Desktop\ScriptKiddie_AV-main\LICENSE %userprofile%\Desktop\SC_AV\
 
